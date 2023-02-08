4712e4b5c1f70cafc31af30c92fd214c127e6aee
unknown <njengagichuhi211@gmail.com>
2023-02-08 22:34:49 +0300

    charracter input

--- quiz_1.py
+++ quiz_1.py
@@ -1,2 +1,10 @@
-#Ask the user for a number.
-#Depending on whether the number is even or odd, print out an appropriate message to the user.
+#Create a program that asks the user to enter their name and their age.
+#Print out a message addressed to them that tells them the year that they will turn 100 years old.
+def result():
+    name=input("enter your name:")
+    age=int(input("enter your age:"))
+    current_year=int(2023)
+    date_of_birth=current_year-age
+    results=date_of_birth+int(100)
+    print(f"helloo{ name } you will be a hundred years on { results }")
+result ()
