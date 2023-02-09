64292afc3a69481b76ded3c6babe339b39b9b052
unknown <njengagichuhi211@gmail.com>
2023-02-09 22:28:22 +0300

    List Less Than Ten

---
+++ quiz3.py
@@ -0,0 +1,9 @@
+#Take a list, say for example this one:
+#and write a program that prints out all the elements of the list that are less than 5
+def less_than(a):
+    for num in a:
+        if num <5:
+            print(num)
+
+less_than([1,2,3,4,5,6,7,9])
+
