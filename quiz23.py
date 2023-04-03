0e8f91c246fba53cfc26826923831f7a6d10e79e
unknown <njengagichuhi211@gmail.com>
2023-04-03 11:41:26 +0300

    Read From File

---
+++ quiz23.py
@@ -0,0 +1,7 @@
+#Given a .txt file that has a list of a bunch of names, count how many of each name there
+#are in the file, and print out
+#the results to the screen. I have a .txt file for you, if you want to use it!
+text_file=open("C:\\Users\HP\\Downloads\\nameslist.txt",'r')
+for count, line in enumerate(text_file):
+    pass
+print('Total Lines', count + 1)
